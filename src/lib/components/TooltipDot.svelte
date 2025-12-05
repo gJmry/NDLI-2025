964c3ecf669542be5d0dcd99e222b927af76f72d
AlecPts <alecpetitsiejak@gmail.com>
2025-12-05 03:42:40 +0100

    Add main page

---
+++ src/lib/components/TooltipDot.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let title;
+    export let desc;
+    export let route;
+    export let x;
+    export let y;
+
+    function go() {
+        location.assign('http://localhost:5173/' + route);
+    }
+</script>
+
+<div style="position: absolute; top:{y}px; left:{x}px;">
+    <div class="wrapper relative w-lg">
+        <input type="button" class="circle" on:click={go}>
+
+        <div class="tooltip absolute bottom-5 bg-green-300 hidden">
+            <strong>{title}</strong>
+            <p>{desc}</p>
+        </div>
+    </div>
+</div>
+
+
+<style>
+    .circle {
+        position: absolute;
+        width: 25px;
+        height: 25px;
+        background-color: red;
+        border-radius: 50%;
+        cursor: pointer;
+    }
+
+    .tooltip {
+        padding: 10px;
+        border-radius: 6px;
+    }
+
+    .wrapper:hover .tooltip {
+        display: block;
+    }
+</style>
---
+++ src/routes/village/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+    import village from '$lib/assets/village-gaulois.webp';
+    import TooltipDot from "$lib/components/TooltipDot.svelte";
+
+    let tooltipDots = [
+        {
+            id: 1,
+            title: "Jeu n°1 - Libre ou pas libre",
+            desc: "Ceci est le premier jeu !",
+            route: "libre-ou-pas-libre",
+            x: 990,
+            y: 210
+        },
+        {
+            id: 2,
+            title: "Jeu n°2 - Attrape le menhir",
+            desc: "Ceci est le deuxième jeu !",
+            route: "attrape-le-menhir",
+            x: 830,
+            y: 560
+        },
+        {
+            id: 3,
+            title: "Jeu n°3 - Empire romain",
+            desc: "Ceci est le troisième jeu !",
+            route: "empire-romain",
+            x: 420,
+            y: 460
+        },
+        {
+            id: 4,
+            title: "Jeu n°4 - Quiz",
+            desc: "Ceci est le quatrième jeu !",
+            route: "quiz",
+            x: 1365,
+            y: 520
+        }
+    ];
+</script>
+
+<main class="relative flex justify-center w-full">
+    <img src="{village}" alt="Village gaulois" class="h-screen"/>
+
+    {#each tooltipDots as dot}
+        <TooltipDot
+                title="{dot.title}"
+                desc="{dot.desc}"
+                route="{dot.route}"
+                x={dot.x}
+                y={dot.y}
+        />
+    {/each}
+</main>
